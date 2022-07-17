810cb768229447e424880ea192f932ae72b46958
Abhijeet Kinjawadekar <abhijeetgk@gmail.com>
2022-07-17 11:56:02 +0530

    django views and urls

--- django/monthly_challenges/challenges/urls.py
+++ django/monthly_challenges/challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:month>',views.monthly_challenges)
+    path("", views.index),
+    path('<str:month>',views.monthly_challenges, name="month_challenge")
 ]
--- django/monthly_challenges/challenges/views.py
+++ django/monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.http.response import Http404, HttpResponseNotFound
 from django.shortcuts import HttpResponse,render
+from django.urls import reverse
 
 # Create your views here.
 monthly_challenge_text = {
@@ -7,6 +8,14 @@ monthly_challenge_text = {
     "february":"This is Feb challenge",
     "march": "This is march challenge"
 }
+def index(request):
+    month_keys = monthly_challenge_text.keys();
+    month_string="";
+    for month in month_keys:
+        month_name = month.capitalize();
+        month_url = reverse("month_challenge", args=[month]);
+        month_string+= f"<li><a href=\"{month_url}\">{month_name}</a></li>"
+    return HttpResponse(month_string)
 
 def monthly_challenges(request, month):
     challenge_text=None;

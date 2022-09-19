71b671370b52254dff31449291e6e42bfe676cd3
Abhijeet Kinjawadekar <abhijeetgk@gmail.com>
2022-09-19 19:19:41 +0530

    further commits from the udemy python course

--- django/my_site/blog/admin.py
+++ django/my_site/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Author, Tag
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ('first_name',)
+
+admin.site.register(Post)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag)
